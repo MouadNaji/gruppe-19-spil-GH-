25b8e2e3e96ba77b031a51f585be88253de0b602
Christian2805 <chri22cm@stud.kea.dk>
2018-11-15 13:14:50 +0100

    m

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function sidenVises() {
 function showStart() {
     console.log("showStart");
     document.querySelector("#playknap").classList.add("pulse");
-    document.querySelector("#game_background").classList.remove("hide");
+    document.querySelector("#start").classList.remove("hide");
 
 }
 //klik på start knap
@@ -20,6 +20,22 @@ document.querySelector("#playknap").addEventListener("click", hideStart);
 
 function hideStart() {
     console.log("hideStart");
-    document.querySelector("#playknap").classList.remove("pulse")
+    //fjern playknap animation.
+    document.querySelector("#playknap").classList.remove("pulse");
+    //forsvindings animation på start siden
+    document.querySelector("#start").classlist.add("hide-start");
+}
+document.querySelector("#start").addEventListener("animationend", startGame);
+
+function startGame() {
+    console.log("startGame");
+    document.querySelector("#start").classList.add("hide");
+    document.querySelector("#game-background").classList.remove("hide");
+
+
+
+}
+
+function hitBottom() {
 
 }
