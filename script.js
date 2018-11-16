0fdd5706d6249cfcd56a596710d182057fe6edb5
Christian2805 <chri22cm@stud.kea.dk>
2018-11-16 11:35:49 +0100

    Random generator virker 100%

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.addEventListener("load", sidenVises);
 let timeLeft = 20;
-let billedeNrOld = NaN;
-let billedNr = NaN;
+let oldRando = 800;
+let randoArr = [1, 2, 3, 4, 5, 6, 7];
+let points = 0;
 
 function sidenVises() {
     console.log("sidenVises");
@@ -33,13 +34,13 @@ function startGame() {
     //    document.querySelector("#gameOver1").classList.add("hide");
     //    document.querySelector("#gameOver2").classList.add("hide");
     document.querySelector("#start").classList.add("hide");
-    document.querySelector("#billede1").addEventListener("click", tjek);
+    document.querySelector("#billede1").addEventListener("click", pointsTjek);
     document.querySelector("#billede1").addEventListener("animationend", gameOver2);
-    document.querySelector("#billede2").addEventListener("click", tjek);
+    document.querySelector("#billede2").addEventListener("click", pointsTjek);
     document.querySelector("#billede2").addEventListener("animationend", gameOver2);
-    document.querySelector("#billede3").addEventListener("click", tjek);
+    document.querySelector("#billede3").addEventListener("click", pointsTjek);
     document.querySelector("#billede3").addEventListener("animationend", gameOver2);
-    document.querySelector("#billede4").addEventListener("click", tjek);
+    document.querySelector("#billede4").addEventListener("click", pointsTjek);
     document.querySelector("#billede4").addEventListener("animationend", gameOver2);
     document.querySelector("#billede5").addEventListener("click", gameOver1);
     document.querySelector("#billede5").addEventListener("animationend", tjek);
@@ -47,11 +48,34 @@ function startGame() {
     document.querySelector("#billede6").addEventListener("animationend", tjek);
     document.querySelector("#billede7").addEventListener("click", gameOver1);
     document.querySelector("#billede7").addEventListener("animationend", tjek);
-    //    startTime();
+    startTime();
     tjek();
 
 }
 //  fjerner animation og synlighed
+
+function pointsTjek() {
+    points++;
+    document.querySelector("#points").innerHTML = points;
+    console.log("points " + points);
+    document.querySelector("#billede1").classList.remove("swoop");
+    document.querySelector("#billede2").classList.remove("swoop");
+    document.querySelector("#billede3").classList.remove("swoop");
+    document.querySelector("#billede4").classList.remove("swoop");
+    document.querySelector("#billede5").classList.remove("swoop");
+    document.querySelector("#billede6").classList.remove("swoop");
+    document.querySelector("#billede7").classList.remove("swoop");
+    document.querySelector("#billede1").classList.add("hide");
+    document.querySelector("#billede2").classList.add("hide");
+    document.querySelector("#billede3").classList.add("hide");
+    document.querySelector("#billede4").classList.add("hide");
+    document.querySelector("#billede5").classList.add("hide");
+    document.querySelector("#billede6").classList.add("hide");
+    document.querySelector("#billede7").classList.add("hide");
+    random();
+
+}
+
 function tjek() {
     document.querySelector("#billede1").classList.remove("swoop");
     document.querySelector("#billede2").classList.remove("swoop");
@@ -67,59 +91,105 @@ function tjek() {
     document.querySelector("#billede5").classList.add("hide");
     document.querySelector("#billede6").classList.add("hide");
     document.querySelector("#billede7").classList.add("hide");
-    billedeAnimation();
+    random();
+}
+
+function random() {
+    if (oldRando == 800) {
+        var randoNUM = Math.ceil(Math.random() * 6);
+
+
+    } else {
+
+
+        randoArr.splice(oldRando - 1, 1);
+
+        var randoNUM = Math.floor(Math.random() * 5);
+
+
+
+    }
+    console.log("=====================================");
+    console.log("randoArr: " + randoArr);
+
+    let rando = randoArr[randoNUM];
+
+
+
+    console.log("olando: " + oldRando);
+
+    oldRando = rando;
+    console.log("det vi vil ha rando: " + rando);
+    document.querySelector("#billede" + rando).classList.add("swoop");
+    document.querySelector("#billede" + rando).classList.remove("hide");
+    randoArr = [1, 2, 3, 4, 5, 6, 7];
+
 }
 
 // --------------laver et nyt billede
-function billedeAnimation() {
-    console.log("billedeAnimation")
-    billedeNr = Math.floor((Math.random() * 7) + 1);
-    console.log(billedeNr);
-    document.querySelector("#billede" + billedeNr).classList.remove("hide");
-    document.querySelector("#billede" + billedeNr).classList.add("swoop"); {
-        billedNrOld = billedNr;
-        console.log(billedeNr);
-    }
-    if (billedeNr == billedeNrOld) {
-        nytNr();
-
-    }
-
-
-
-    function nytNr() {
-        console.log("lort");
-        billedeNr = Math.floor((Math.random() * 7) + 1);
-        console.log("old" + billedeNrOld);
-
-
-
-    }
-
-}
-
-//-------Timer-------
-
-//function startTime() {
-//    timeLeft--;
-//    console.log(timeLeft);
-//
-//    if (timeLeft > 0) {
-//
-//        gameTimeOut = setTimeout(startTime, 1000);
+//function billedeAnimation() {
+//    console.log("billedeAnimation")
+//    billedeNr = Math.floor((Math.random() * 2) + 1);
+//    console.log(billedeNr);
 //
 //
+//    function nytNr() {
+//        console.log("Marias funktion")
+//        billedeNrOld = billedeNr;
+//        billedeNr = Math.floor((Math.random() * 2) + 1);
+//        console.log(billedeNrOld);
+//    }
+//    if (billedeNr == billedeNrOld) {
+//        nytNr();
 //    } else {
-//        levelComplete();
+//        document.querySelector("#billede" + billedeNr).classList.remove("hide");
+//        document.querySelector("#billede" + billedeNr).classList.add("swoop");
+//        document.querySelector("#billede" + billedeNr).addEventListener("animationend", nytNr);
+//
+//
+//    }
+
+//{
+//        billedNrOld = billedNr;
+//        console.log(billedeNr);
+//    }
+//    if (billedeNr == billedeNrOld) {
+//        nytNr();
+//
 //    }
 //
-//    document.querySelector("#tid").textContent = timeLeft;
-//}
+//
+//
+//    function nytNr() {
+//        console.log("lort");
+//        billedeNr = Math.floor((Math.random() * 7) + 1);
+//        console.log("old" + billedeNrOld);
+//
+//
+//
+//    }
+
+
+
+//-- -- -- - Timer-- -- -- -
+
+function startTime() {
+    timeLeft--;
+    console.log(timeLeft);
+
+    if (timeLeft > 0) {
+
+        gameTimeOut = setTimeout(startTime, 1000);
+
+
+    } else {
+        levelComplete();
+    }
+
+    document.querySelector("#tid").textContent = timeLeft;
+}
 
 function hitBottom() {
-
-
-
 
 }
 
